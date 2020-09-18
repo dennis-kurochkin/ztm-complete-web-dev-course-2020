e01b549402c9a582e0bd22d403ee11c76dc02147
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-09-18 14:50:19 +0300

    Refactor project structure

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
 import './App.css';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
   constructor() {
@@ -38,7 +39,9 @@ class App extends Component {
       <div className="tc">
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
