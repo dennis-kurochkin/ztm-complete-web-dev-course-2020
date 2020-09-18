bbbc9ef7536319d68590d4e2bc13fa0bb978a83f
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-09-18 14:56:45 +0300

    Refactor

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -27,23 +27,23 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(
-      elem => elem.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    const { robots, searchField } = this.state;
+
+    const filteredRobots = robots.filter(
+      robot => robot.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>
-    }
-
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
+    return !robots.length ?
+      <h1>Loading...</h1> :
+      (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
   }
 }
 
